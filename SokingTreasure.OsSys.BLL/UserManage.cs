f671e7678f4e8c0286fe7dc09d1b605ffca4f70e
朱亚涛 <40811911+veryCoolFirst@users.noreply.github.com>
2018-12-18 13:03:20 +0800

    公共库（加密）
    用户数据验证

--- SokingTreasure.OsSys.BLL/UserManage.cs
+++ SokingTreasure.OsSys.BLL/UserManage.cs
@@ -1,4 +1,5 @@
-﻿using SokingTreasure.OsSys.Models;
+﻿using SokingTreasure.OsSys.DAL;
+using SokingTreasure.OsSys.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,23 @@ using System.Threading.Tasks;
 
 namespace SokingTreasure.OsSys.BLL
 {
+    /// <summary>
+    /// 用户管理
+    /// </summary>
     public class UserManage
     {
-
+        /// <summary>
+        /// 检查用户(登录)
+        /// </summary>
+        /// <param name="user">用户信息</param>
+        /// <returns></returns>
+        public static bool CheckUser(UserLogin user)
+        {
+            return UserCRUD.CheckUser(user);
+        }
+        public static UserLogin GetWhereByUser(UserLogin user)
+        {
+            return UserCRUD.GetWhereByUser(user);
+        }
     }
 }
---
+++ SokingTreasure.OsSys.DAL/UserCRUD.cs
@@ -0,0 +1,56 @@
+﻿using SokingTreasure.OsSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokingTreasure.OsSys.DAL
+{
+    /// <summary>
+    /// 用户(增删查改)
+    /// </summary>
+    public class UserCRUD
+    {
+        /// <summary>
+        /// 检查用户(登录)
+        /// </summary>
+        /// <param name="user">用户信息</param>
+        /// <returns></returns>
+        public static bool CheckUser(UserLogin user) {
+            string sql = $"select * from UserLogin where LoginName=@LoginName and LoginPwd=@LoginPwd";
+            SqlParameter[] param = {
+                 new SqlParameter("@LoginName",user.LoginName),
+                 new SqlParameter("@LoginPwd",user.LoginPwd)
+             };
+            SqlDataReader reader = DbHelper.ExectueReader(sql, false, param);
+            while (reader.Read())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static UserLogin GetWhereByUser(UserLogin user)
+        {
+            string sql = $"select * from UserLogin where LoginName=@LoginName and LoginPwd=@LoginPwd";
+            SqlParameter[] param = {
+                 new SqlParameter("@LoginName",user.LoginName),
+                 new SqlParameter("@LoginPwd",user.LoginPwd)
+             };
+            DataTable table = DbHelper.GetDataTable(sql, false,param);
+            UserLogin ul = new UserLogin();
+            foreach (DataRow item in table.Rows)
+            {
+                ul.Id = (int)item["Id"];
+                ul.LoginName = item["LoginName"].ToString();
+                ul.LoginPwd = item["LoginPwd"].ToString();
+                ul.LoginEmail = item["LoginEmail"].ToString();
+                ul.StateId = (int)item["StateId"];
+            }
+            return ul;
+        }
+    }
+}
